ba5b442c68809e201eacb272b1bfa53069a1dcca
Eka <eka.abashidze@gmail.com>
2022-11-24 16:22:01 +0400

    project setup

---
+++ src/components/Marginals.js
@@ -0,0 +1,21 @@
+function Marginals() {
+    return (
+        <div>
+        <svg
+            
+            width="48"
+            height="48"
+            viewBox="0 0 48 48"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <rect width="48" height="48" fill="#030303" />
+            <circle cx="24" cy="24" r="10.5" stroke="#FFF7F0" stroke-width="3" />
+        </svg>
+
+        <h1>Free Consultation</h1>
+        </div>
+    );
+}
+
+export default Marginals;
